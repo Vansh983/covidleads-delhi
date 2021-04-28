73061fb0e8d06e0e2cdf3bb13595cc6711fcb585
Vansh Sood <emailofvansh@gmail.com>
2021-04-28 07:41:31 +0530

    backend done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
+import { Cards, Chart, CountryPicker, ItemCard } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import MenuBar from "./components/MenuBar/MenuBar";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
 
-    state = {
-        data: {},
-    }
+  async componentDidMount() {
+    const fetchedData = await fetchData();
 
-    async componentDidMount(){
-        const fetchedData = await fetchData();
+    this.setState({
+      data: fetchedData,
+    });
+  }
 
-        this.setState({
-            data: fetchedData
-        });
-    }
+  render() {
+    const { data } = this.state;
 
-    render(){
-        const { data } = this.state;
-
-        return (
-            <div className={styles.container}>
-                <Cards data={data} />
-
-            </div>
-        )
-    }
+    return (
+      <div className={styles.container}>
+        {/*<Cards data={data} />*/}
+        <MenuBar />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api';
+const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
-    try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
 
-        const modifiedData = {
-            confirmed,
-            recovered,
-            deaths,
-            lastUpdate,
-        }
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
 
-        return modifiedData;
-
-    } catch (error){
-
-    }
-}
+    return modifiedData;
+  } catch (error) {}
+};
 
 export const fetchDailyData = async () => {
-    try {
-        const { data } = await axios.get(`${url}/daily`);
-
-
-    } catch (error) {
-        
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+  } catch (error) {}
+};
