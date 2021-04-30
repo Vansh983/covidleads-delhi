7643dbde000755850809fc86d3fcde0702d65a36
Vansh Sood <emailofvansh@gmail.com>
2021-04-30 09:23:34 +0530

    nostock

--- components/Beds/Beds.js
+++ components/Beds/Beds.js
@@ -6,13 +6,17 @@ import BedCard from "./BedCard";
 function Beds({ beds, category }) {
   return (
     <div style={{ marginTop: "20px" }}>
-      <Grid container spacing={2} justify="center">
-        {beds.map((bed, index) => (
-          <Grid item md={6} xs={10}>
-            <BedCard key={index} category={category} bed={bed} />
-          </Grid>
-        ))}
-      </Grid>
+      {beds.length ? (
+        <Grid container spacing={2} justify="center">
+          {beds.map((bed, index) => (
+            <Grid item md={6} xs={10}>
+              <BedCard key={index} category={category} bed={bed} />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <h2>We Don't have any leads for {category} right now.</h2>
+      )}
     </div>
   );
 }
