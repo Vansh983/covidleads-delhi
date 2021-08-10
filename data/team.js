bf1922aa4ee327796555a5ad1800be5ddc9eb7d4
Vansh Sood <emailofvansh@gmail.com>
2021-08-10 14:41:47 +0530

    about us page

---
+++ data/team.js
@@ -0,0 +1,26 @@
+export const team = [
+  {
+    id: 1,
+    name: "Vansh Sood",
+    img: "/vansh.jpg",
+    title: "Founder",
+  },
+  {
+    id: 2,
+    name: "Neha Sood",
+    img: "/neha.jpeg",
+    title: "Co Founder",
+  },
+  {
+    id: 3,
+    name: "Anushree Gogia",
+    img: "/anu.jpeg",
+    title: "Co Founder",
+  },
+  {
+    id: 4,
+    name: "Raunaq Arora",
+    img: "/raunaq.jpeg",
+    title: "Head of External Relations",
+  },
+];
